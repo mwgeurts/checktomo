30b79f760d8d5655882699775fc31fbb6ca85d34
Mark Geurts <mark.w.geurts@gmail.com>
2017-05-09 10:03:46 -0500

    Added Y and Z line plots

--- private/UpdateLineDisplay.m
+++ private/UpdateLineDisplay.m
@@ -25,7 +25,7 @@ elseif nargin == 1
     % Set input variables
     handles = varargin{1};
     update = 1;
-    
+
 % Otherwise, if 2, set the handles/update flags and update the plot
 elseif nargin == 2
     
@@ -33,32 +33,21 @@ elseif nargin == 2
     handles = varargin{1};
     update = varargin{2};
 
-    % Start timer
-    tic;
-    
 % Otherwise, throw an error
 else 
     Event('Incorrect number of inputs to UpdateLineDisplay', 'ERROR');
 end
 
+% Start timer
+tic;
+
 % If the update flag is passed
 if update == 1
     
-    % Update TCS display
-    switch get(handles.line_menu, 'Value')
-
-        % X Profile
-        case 1
-            handles.transverse.Update;
-
-        % Y Profile
-        case 2
-            handles.coronal.Update;
-
-        % Z Profile
-        case 3
-            handles.sagittal.Update;
-    end
+    % Update all profiles
+    handles.transverse.Update;
+    handles.coronal.Update;
+    handles.sagittal.Update;
 end
 
 % Clear and set reference to axis
@@ -74,6 +63,9 @@ set(handles.line_slider, 'visible', 'off');
 % If data exists
 if isfield(handles, 'referenceDose') && ...
         isfield(handles.referenceDose, 'data')
+    
+    % Store maximum axes dimension
+    m = max(handles.referenceDose.dimensions);
     
     % Execute code block based on display GUI item value
     switch get(handles.line_menu, 'Value')
@@ -180,27 +172,235 @@ if isfield(handles, 'referenceDose') && ...
             hold on;
             plot([min(x) max(x)], [-(handles.referenceDose.start(2) + ...
                 handles.referenceDose.width(2) * ...
-                (handles.referenceDose.dimensions(1) - ...
-                round(get(handles.line_slider, 'Value')))) ...
+                (m - round(get(handles.line_slider, 'Value')) + 0.5)) ...
                 -(handles.referenceDose.start(2) + ...
                 handles.referenceDose.width(2) * ...
-                (handles.referenceDose.dimensions(1) - ...
-                round(get(handles.line_slider, 'Value'))))], ...
+                (m - round(get(handles.line_slider, 'Value')) + 0.5))], ...
                 'Color', 'white', 'LineWidth', 1);
             hold off;
             
         % Y Profile
         case 2
 
+            % Store x coordinates
+            x = handles.referenceDose.start(3):...
+                handles.referenceDose.width(3):...
+                handles.referenceDose.start(3) + ...
+                handles.referenceDose.width(3) * ...
+                (handles.referenceDose.dimensions(3) - 1);
+            
+            % If slider min/max do not match this axis, update it (along
+            % with the value)
+            if (get(handles.line_slider, 'Max') - ...
+                    get(handles.line_slider, 'Min') + 1) ~= ...
+                    handles.referenceDose.dimensions(1)
+                
+                % Update slider Min/Max
+                set(handles.line_slider, 'Min', 1);
+                set(handles.line_slider, 'Max', ...
+                    handles.referenceDose.dimensions(1));
+                
+                % Update value
+                set(handles.line_slider, 'Value', ...
+                    floor(handles.referenceDose.dimensions(1) / 2));
+                
+                % Update slider minor/major steps
+                set(handles.line_slider, 'SliderStep', [1 / ...
+                    (handles.referenceDose.dimensions(1) - 1) ...
+                    10 / handles.referenceDose.dimensions(1)]);
+            end
+            
+            % Turn on slider
+            set(handles.line_slider, 'Visible', 'on');
+            
+            % Plot line profile along IEC X at slider position
+            plot(x, squeeze(handles.referenceDose.data(...
+                round(get(handles.line_slider, 'Value')), ...
+                round(get(handles.cor_slider, 'Value')), :)), '-b');
+            
+            % Add legend entry
+            l{1} = 'Planned';
+            
+            % Plot secondary dose, if it exists
+            if isfield(handles, 'secondDose') && ...
+                    isfield(handles.secondDose, 'data')
 
+                % Hold plot build
+                hold on;
+                
+                % Plot line profile along IEC X at slider position
+                plot(x, squeeze(handles.secondDose.data(...
+                    round(get(handles.line_slider, 'Value')), ...
+                    round(get(handles.cor_slider, 'Value')), :)), '--b');
+                
+                % Add legend entry
+                l{2} = 'Re-calculated';
+                
+                % Resume plot build
+                hold off;
+            end
+            
+            % Plot Gamma profile, if it exists
+            if isfield(handles, 'gamma') && ~isempty(handles.gamma)
+
+                % Hold plot build
+                hold on;
+                
+                % Plot Gamma against second Y axis
+                yyaxis right
+                
+                % Plot line profile along IEC X at slider position
+                plot(x, squeeze(handles.gamma(...
+                    round(get(handles.line_slider, 'Value')), ...
+                    round(get(handles.cor_slider, 'Value')), :)), '-.r');
+                
+                % Add legend entry
+                l{3} = 'Gamma';
+                
+                % Resume plot build
+                hold off;
+                
+                % Adjust second axis display
+                set(handles.line_axes, 'YColor', 'black');
+                ylabel('Gamma Index');
+                ylim([0 2]);
+                
+                % Go back to left axis
+                yyaxis left
+            end
+            
+            % Adjust plot display
+            xlim([min(x) max(x)]);
+            xlabel('Y Axis Position (cm)');
+            ylabel('Dose (Gy)');
+            grid on;
+            legend(l, 'Location', 'southwest');
+            
+            % Display line on TCS plot for current slice position
+            axes(handles.cor_axes);
+            hold on;
+            plot([-(handles.referenceDose.start(1) + ...
+                handles.referenceDose.width(1) * ...
+                (m - round(get(handles.line_slider, 'Value')))) ...
+                -(handles.referenceDose.start(1) + ...
+                handles.referenceDose.width(1) * ...
+                (m - round(get(handles.line_slider, 'Value'))))], ...
+                [min(x) max(x)], 'Color', 'white', 'LineWidth', 1);
+            hold off;
+            
         % Z Profile
         case 3
 
-    end
-end
+            % Store x coordinates
+            x = handles.referenceDose.start(2):...
+                handles.referenceDose.width(2):...
+                handles.referenceDose.start(2) + ...
+                handles.referenceDose.width(2) * ...
+                (handles.referenceDose.dimensions(2) - 1);
+            
+            % If slider min/max do not match this axis, update it (along
+            % with the value)
+            if (get(handles.line_slider, 'Max') - ...
+                    get(handles.line_slider, 'Min') + 1) ~= ...
+                    handles.referenceDose.dimensions(3)
+                
+                % Update slider Min/Max
+                set(handles.line_slider, 'Min', 1);
+                set(handles.line_slider, 'Max', ...
+                    handles.referenceDose.dimensions(3));
+                
+                % Update value
+                set(handles.line_slider, 'Value', ...
+                    floor(handles.referenceDose.dimensions(3) / 2));
+                
+                % Update slider minor/major steps
+                set(handles.line_slider, 'SliderStep', [1 / ...
+                    (handles.referenceDose.dimensions(3) - 1) ...
+                    10 / handles.referenceDose.dimensions(3)]);
+            end
+            
+            % Turn on slider
+            set(handles.line_slider, 'Visible', 'on');
+            
+            % Plot line profile along IEC X at slider position
+            plot(x, squeeze(handles.referenceDose.data(...
+                round(get(handles.sag_slider, 'Value')), :, ...
+                round(get(handles.line_slider, 'Value')))), '-b');
+            
+            % Add legend entry
+            l{1} = 'Planned';
+            
+            % Plot secondary dose, if it exists
+            if isfield(handles, 'secondDose') && ...
+                    isfield(handles.secondDose, 'data')
 
-% Clear temporary profiles
-clear x;
+                % Hold plot build
+                hold on;
+                
+                % Plot line profile along IEC X at slider position
+                plot(x, squeeze(handles.secondDose.data(...
+                    round(get(handles.sag_slider, 'Value')), :, ...
+                    round(get(handles.line_slider, 'Value')))), '--b');
+                
+                % Add legend entry
+                l{2} = 'Re-calculated';
+                
+                % Resume plot build
+                hold off;
+            end
+            
+            % Plot Gamma profile, if it exists
+            if isfield(handles, 'gamma') && ~isempty(handles.gamma)
+
+                % Hold plot build
+                hold on;
+                
+                % Plot Gamma against second Y axis
+                yyaxis right
+                
+                % Plot line profile along IEC X at slider position
+                plot(x, squeeze(handles.gamma(...
+                    round(get(handles.sag_slider, 'Value')), :, ...
+                    round(get(handles.line_slider, 'Value')))), '-.r');
+                
+                % Add legend entry
+                l{3} = 'Gamma';
+                
+                % Resume plot build
+                hold off;
+                
+                % Adjust second axis display
+                set(handles.line_axes, 'YColor', 'black');
+                ylabel('Gamma Index');
+                ylim([0 2]);
+                
+                % Go back to left axis
+                yyaxis left
+            end
+            
+            % Adjust plot display
+            xlim([min(x) max(x)]);
+            xlabel('Z Axis Position (cm)');
+            ylabel('Dose (Gy)');
+            grid on;
+            legend(l, 'Location', 'southwest');
+            
+            % Display line on TCS plot for current slice position
+            axes(handles.sag_axes);
+            hold on;
+            plot([min(x) max(x)], [handles.referenceDose.start(3) + ...
+                handles.referenceDose.width(3) * ...
+                (round(get(handles.line_slider, 'Value')) - 1) ...
+                handles.referenceDose.start(3) + ...
+                handles.referenceDose.width(3) * ...
+                (round(get(handles.line_slider, 'Value')) - 1)], ...
+                'Color', 'white', 'LineWidth', 1);
+            hold off;
+    end
+    
+    % Clear temporary profiles
+    clear x m;
+end
 
 % Log completion
 Event(sprintf('Plot updated successfully in %0.3f seconds', toc));
