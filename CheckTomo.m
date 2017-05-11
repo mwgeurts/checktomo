19e0daea9fb273656f99f21cc4c13d1861f4f084
Mark Geurts <mark.w.geurts@gmail.com>
2017-05-11 14:57:19 -0500

    Packaged version 1.0.0

--- CheckTomo.m
+++ CheckTomo.m
@@ -91,7 +91,7 @@ warning('off', 'all');
 handles.output = hObject;
 
 % Set version handle
-handles.version = '0.1';
+handles.version = '1.0.0';
 
 % Determine path of current application
 [path, ~, ~] = fileparts(mfilename('fullpath'));
